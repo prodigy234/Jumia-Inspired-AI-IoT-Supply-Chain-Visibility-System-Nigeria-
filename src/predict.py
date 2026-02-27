a92594fb9419e3ae80ef2219ee906a3711615d35
prodigy234 <k.gbenga234@gmail.com>
2026-02-27 13:51:55 +0100

    my third commit

--- src/predict.py
+++ src/predict.py
@@ -1,10 +1,42 @@
+import os
 import joblib
 import pandas as pd
 
-model = joblib.load("models/delay_model.pkl")
+MODEL_PATH = os.path.join(
+    os.path.dirname(os.path.dirname(__file__)),
+    "models",
+    "delay_model.pkl"
+)
+
+model = joblib.load(MODEL_PATH)
+
+FEATURE_ORDER = [
+    'Delivery_Distance_km',
+    'Delivery_Days',
+    'Shipment_Temperature_C',
+    'Traffic_Level_Encoded',
+    'Courier_Type',
+    'Warehouse_Hub',
+    'State',
+    'City',
+    'Category',
+    'Payment_Method'
+]
 
 def predict_delay(static_features, iot_features):
     data = {**static_features, **iot_features}
     df = pd.DataFrame([data])
+    df = df[FEATURE_ORDER]
     risk = model.predict_proba(df)[0][1]
     return risk
+
+# import joblib
+# import pandas as pd
+
+# model = joblib.load("models/delay_model.pkl")
+
+# def predict_delay(static_features, iot_features):
+#     data = {**static_features, **iot_features}
+#     df = pd.DataFrame([data])
+#     risk = model.predict_proba(df)[0][1]
+#     return risk
